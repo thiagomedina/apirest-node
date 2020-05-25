062d9302d51bb361121baa333e4c31cabc25a80e
thiagomedina <thiagomedina001@gmail.com>
2020-05-25 00:04:44 -0300

    sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  username: 'thiago',
+  password: 'root',
+  database: 'cotacao',
+  host: 'localhost',
+  dialect: 'mysql',
+  define: {
+    timestamps: true,
+    undersored: true,
+    undersoredAll:true
+  }
+
+
+}
